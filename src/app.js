31d685f836a23ad5526f874239f7ddee78795a9f
Lucia Calvo <lucalvo01@hotmail.com>
2023-07-31 18:59:00 -0300

    Primera pre entrega

---
+++ src/app.js
@@ -0,0 +1,16 @@
+//Importamos las rutas
+import express from "express";
+import productsRouter from "./routes/products.router.js";
+import cartRouter from "./routes/carts.router.js";
+
+const app = express();
+const puerto = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use("/api/products/", productsRouter);
+app.use("/api/carts/", productsRouter);
+
+app.listen(puerto, () => {
+    console.log(`Servidor corriendo en el puerto ${puerto}`);
+})
