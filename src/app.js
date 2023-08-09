1aa41491ca272d4c7716f60863e8fad444079895
Lucia Calvo <lucalvo01@hotmail.com>
2023-08-08 21:20:27 -0300

    handlebars

--- src/app.js
+++ src/app.js
@@ -2,14 +2,42 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartRouter from "./routes/carts.router.js";
+import ProductManager from "./controllers/productManager.js";
+
+import { engine } from "express-handlebars"
+import * as path from "path"
+import __dirname from "./utils.js";
 
 const app = express();
 const puerto = 8080;
+const product = new ProductManager()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/api/products/", productsRouter);
-app.use("/api/carts/", productsRouter);
+app.use("/api/carts/", cartRouter);
+
+//Estructura handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname + "/views"))
+
+//Archivos estaticos
+app.use("/", express.static(__dirname + "/public"))
+
+app.get("/", async (req,res) => {
+    let allProducts = await product.getProducts()
+    res.render("home",{
+        title:"Desafio Handlebars",
+        products: allProducts
+    })
+})
+
+app.post("/products", async (req, res) => {
+    let newProduct = req.body
+    res.send(await product.writeProducts(newProduct))
+})
+
 
 app.listen(puerto, () => {
     console.log(`Servidor corriendo en el puerto ${puerto}`);
